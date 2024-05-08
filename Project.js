d19911b71aa2ac538849566a89be968d922c9c3b
kiran <Sanjaychr2001@gmail.com>
2024-05-08 22:45:39 +0530

    TO DO List

---
+++ Project.js
@@ -0,0 +1,18 @@
+let students = [
+    { name: "Smith", rollNumber: 31, marks: 80 },
+    { name: "Jenny", rollNumber: 15, marks: 69 },
+    { name: "John", rollNumber: 16, marks: 35 },
+    { name: "Tiger", rollNumber: 7, marks: 55 }
+];
+let x=students.filter((element,index)=>{
+    
+        element =>"marks"=50;
+    
+
+    
+
+
+});
+console.log(x);
+
+
